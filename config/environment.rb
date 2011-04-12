4b7c02d9b3fc714e7f0eddce0dee20f80bebc310
Nicolas <nicolas.papon@webflows.fr>
2011-04-12 14:16:22 +0200

    des petits trucs a droite a gauche

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
 # Used to bypass certificate checking when downloading jquery from github using the gem jquery_rails
-require 'openssl'
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+#require 'openssl'
+#OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 
 # Load the rails application
