cbf6f59545562855aea38cd8e7ee98f20dc63aed
Nicolas <nicolas.papon@webflows.fr>
2011-04-12 17:30:18 +0200

    bugfix

--- app/controllers/financial_data_controller.rb
+++ app/controllers/financial_data_controller.rb
@@ -3,6 +3,6 @@ class FinancialDataController < ApplicationController
   active_scaffold :financial_data do |config|
     config.columns = [:entity, :year, :turnover, :ebe, :profit, :nb_employees]
     config.columns[:profit].options[:format] = :percentage
-    config.columns[:year].options[:format] = config.columns[:turnover].options[:format] = config.columns[:year].options[:ebe] = :currency
+    config.columns[:turnover].options[:format] = config.columns[:year].options[:ebe] = :currency
   end
 end
