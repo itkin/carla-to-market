0bf43dcbece8cb08b7b16ab7357053ffea513373
Nicolas <nicolas.papon@webflows.fr>
2011-04-18 16:03:06 +0200

    ajout d'un onglet utilisateurs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
   active_scaffold :users do |config|
-    config.columns = [:email]
+    config.list.columns = config.show.columns = [:email]
+    config.create.columns = config.update.columns = [:email, :password]
     record_select :per_page => 3, :search_on => [:email], :order_by => 'email'
   end
 end
